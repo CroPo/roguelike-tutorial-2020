22e377376b1b7f5f4761ceac6f3b8605c4f34902
reinmar <mail@reinmar-spindler.at>
2020-07-07 08:32:21 +0200

    Added JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,18 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.hexworks.zircon:zircon.core-jvm:2020.1.6-HOTFIX")
     implementation("org.hexworks.zircon:zircon.jvm.swing:2020.1.6-HOTFIX")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 }
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
+
 
 tasks {
     compileKotlin {
