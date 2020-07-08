9b396e73479c0382ec8ec7bbfc5df20d1786ef7b
reinmar <mail@reinmar-spindler.at>
2020-07-08 16:31:18 +0200

    updated MovementAction

--- docs/Part5_PlacingEnemiesAndKickingThemHarmlessly.md
+++ docs/Part5_PlacingEnemiesAndKickingThemHarmlessly.md
@@ -345,3 +345,36 @@ override fun perform(engine: Engine, entityId: UUID) {
 }
 ```
 
+And a few more changes in the `MovementAction`
+
+
+```kotlin
+override fun perform(engine: Engine, entityId: UUID) {
+    if (!engine.entityEngine.has(entityId, GridPosition::class)) {
+        return
+    }
+
+    val entityPosition = engine.entityEngine.get(entityId, GridPosition::class)
+    val targetPosition = entityPosition!!.position3D.withRelativeX(dx).withRelativeY(dy)
+
+    when {
+        targetPosition.x !in 0 until engine.gameArea.actualSize.xLength
+                || targetPosition.y !in 0 until engine.gameArea.actualSize.yLength
+        -> println(
+            "Can't move beyond the edge of the world"
+        )
+        engine.entityEngine.get(GridPosition::class).filter {
+                (entityId, position) ->
+            position.position3D == targetPosition && engine.entityEngine.has(entityId, GridAttributes::class)
+        }.any {
+            (entityId, _) ->
+            engine.entityEngine.get(entityId, GridAttributes::class)!!.isBlocking
+        } -> println("Walked into a wall. BONK!")
+        else -> {
+            engine.gameArea.fetchBlockAt(entityPosition.position3D).get().removeEntity(entityId)
+            engine.gameArea.fetchBlockAt(targetPosition).get().addEntity(entityId)
+            entityPosition.position3D = targetPosition
+        }
+    }
+}
+```
--- src/main/kotlin/com/cropo/action/MovementAction.kt
+++ src/main/kotlin/com/cropo/action/MovementAction.kt
@@ -1,22 +1,40 @@
 package com.cropo.action
 
 import com.cropo.engine.Engine
+import com.cropo.entity.component.FieldOfView
+import com.cropo.entity.component.GridAttributes
+import com.cropo.entity.component.GridPosition
 import org.hexworks.cobalt.core.api.UUID
 
 /**
  * Move an entity to a specified position
  */
 class MovementAction(val dx: Int = 0, val dy: Int = 0) : Action {
-    override fun perform(engine: Engine, entity: UUID) {
-        val targetPosition = entity.position.withRelativeX(dx).withRelativeY(dy)
+    override fun perform(engine: Engine, entityId: UUID) {
+        if (!engine.entityEngine.has(entityId, GridPosition::class)) {
+            return
+        }
+
+        val entityPosition = engine.entityEngine.get(entityId, GridPosition::class)
+        val targetPosition = entityPosition!!.position3D.withRelativeX(dx).withRelativeY(dy)
 
         when {
-            targetPosition.x !in 0 until engine.gameArea.actualSize.xLength || targetPosition.y !in 0 until engine.gameArea.actualSize.yLength -> println("Can't move beyond the edge of the world")
-            engine.entities.filter { it.position == targetPosition }.any { !it.isWalkable } -> println("Walked into a wall. BONK!")
+            targetPosition.x !in 0 until engine.gameArea.actualSize.xLength
+                    || targetPosition.y !in 0 until engine.gameArea.actualSize.yLength
+            -> println(
+                "Can't move beyond the edge of the world"
+            )
+            engine.entityEngine.get(GridPosition::class).filter {
+                    (entityId, position) ->
+                position.position3D == targetPosition && engine.entityEngine.has(entityId, GridAttributes::class)
+            }.any {
+                (entityId, _) ->
+                engine.entityEngine.get(entityId, GridAttributes::class)!!.isBlocking
+            } -> println("Walked into a wall. BONK!")
             else -> {
-                engine.gameArea.fetchBlockAt(entity.position).get().removeEntity(entity)
-                engine.gameArea.fetchBlockAt(targetPosition).get().addEntity(entity)
-                entity.position = targetPosition
+                engine.gameArea.fetchBlockAt(entityPosition.position3D).get().removeEntity(entityId)
+                engine.gameArea.fetchBlockAt(targetPosition).get().addEntity(entityId)
+                entityPosition.position3D = targetPosition
             }
         }
     }
